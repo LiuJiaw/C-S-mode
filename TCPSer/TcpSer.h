74f4044d3a60a6921e781633497df2d0c1c4c082
哒啦~哒 <37185284+LiuJiaw@users.noreply.github.com>
2018-03-26 20:11:47 +0800

    Update TcpSer.h

--- TCPSer/TcpSer.h
+++ TCPSer/TcpSer.h
@@ -18,23 +18,12 @@ using namespace std;
 #define TIMEFOR_THREAD_EXIT			5000	//主线程睡眠时间
 
 typedef vector<CClient*> ClIENTVECTOR;		//向量容器
-typedef vector<string> SVECTOR;             //内容字符
 
 
-/**
- * 全局变量
- */
 extern char	dataBuf[MAX_NUM_BUF];				//写缓冲区
-extern bool	bConning;							//与客户端的连接状态
 extern bool bSend;                              //发送标记位
-extern bool clientConn;                         //连接客户端标记
-extern SOCKET sServer;							//服务器监听套接字
-extern HANDLE AccHandle;						//数据处理线程句柄
-extern HANDLE CleanHandle;						//数据接收线程
 
-/**
- *函数申明
- */
+
 bool initSever(void);                       //初始化
 void InitGlobal(void);
 bool initSocket(void);						//初始化非阻塞套接字
